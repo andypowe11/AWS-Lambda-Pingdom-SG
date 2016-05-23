bdbb8baec18c5b26e365875998d11bd63655cb09
Andy Powell <andy@andypowe11.net>
2016-05-23 21:43:06 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,90 @@
-# AWS-Lambda-Pingdom-SG
-Update a set of security groups in order to whitelist all the Pingdom probe IP addresses, allowing inbound traffic on ports 80 and 443.
+# AWS Lambda function to generate Pingdom probe Security Groups
+
+[Pingdom](https://pingdom.com/) is an online service that regularly monitors website uptime and performance.
+
+In cases where the website you want to monitor is not publicly accessible, you will need to
+whitelist all of the Pingdom probe IP addresses in order that monitoring can take place.
+
+This AWS Lambda function updates a set of security groups in order to whitelist all the
+Pingdom probe IP addresses, allowing inbound traffic on ports 80 and 443.
+To function properly, the Lamdbda function should be called regularly (at least once per day).
+
+## Usage
+
+### Create a set of security groups
+
+Create a set of AWS security groups, using names of the form 'pingdom1', 'pingdom2', etc.
+Set a **Name** tag, with the value set to the name, and an **AutoUpdate tag**,
+with the value set to 'true'.
+
+There are currently around 70 Pingdom probe IP addresses and each requires 2 security
+group rules - one for port 80 and one for port 443.
+That's around 140 rules.
+Each security group is limited to 50 rules by default, so unless you get the limit
+raised, you'll need to create 3 security groups.
+
+### Create an IAM policy and execution role for the Lambda function
+
+You will create the Lambda execution role that determines the AWS service
+calls that the function is authorized to complete.
+
+Before doing so, you need to create an IAM policy that you will attach to the role.
+
+1. In the IAM console, click **Policies** > **Create Policy** > **Select** (next to **Create Your Own Policy**).
+2. Then supply a name for your policy, and copy and paste the following policy
+into the **Policy Document** box:
+
+```
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:DescribeSecurityGroups",
+        "ec2:AuthorizeSecurityGroupIngress",
+        "ec2:RevokeSecurityGroupIngress"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+```
+
+### Create an execution role
+
+Now that you have created your policy, you can create your Lambda execution role using that policy:
+
+1. In the IAM console, click **Roles** > **Create New Role**, and then name your role.
+2. To select a role type, select **AWS Service Roles** > **AWS Lambda**.
+3. Attach the policy you just created.
+4. After confirming your selections, click **Create Role**.
+
+### Create your Lambda function
+
+Now that you have created your Lambda execution role, you can create your Lambda function:
+
+1. Go to the Lambda console and select **Create a Lambda function**.
+2. Give your Lambda function a name and description, and select **Python 2.7** from the Runtime menu.
+3. Paste the code from `lambda-pingdom-sg.py`.
+4. Below the code window for Lambda function handler and role, select the execution role you created earlier.
+5. Under **Advanced settings**, increase the **Timeout** to 5 seconds.  If you are updating several security groups with this function, you might have to increase the timeout by even more time. Finally, click **Next**.
+6. After confirming your settings are correct, click **Create function**.
+
+## Running your Lambda function
+
+Configure a **Scheduled Event** (under **Event sources**) to run the Lambda function on a daily basis.
+
+## Notes
+
+This Lambda function and associated instructions are heavily based on
+https://blogs.aws.amazon.com/security/post/Tx1LPI2H6Q6S5KC/How-to-Automatically-Update-Your-Security-Groups-for-Amazon-CloudFront-and-AWS-W
---
+++ lambda-pingdom-sg.py
@@ -0,0 +1,126 @@
+'''
+Update a set of security groups in order to whitelist all the Pingdom probe IP
+addresses, allowing inbound traffic on ports 80 and 443. This typically requires
+3 security groups (for ~70 IP addresses, assuming the default limit of 50
+inbound rules per SG and 2 ingress ports per IP address.
+
+Heavily based on https://blogs.aws.amazon.com/security/post/Tx1LPI2H6Q6S5KC/How-to-Automatically-Update-Your-Security-Groups-for-Amazon-CloudFront-and-AWS-W
+
+Andy Powell
+22/05/2016
+'''
+
+import boto3
+import urllib2
+ 
+# URL of the list of Pingdom probe IP addresses to be whitelisted
+SOURCE = "https://my.pingdom.com/probes/ipv4"
+# Ports that need inbound permissions from the Pingdom probes
+INGRESS_PORTS = [ 80, 443 ]
+# Tags which identify the security groups you want to update
+SECURITY_GROUP_TAGS = { 'Name': 'pingdom*', 'AutoUpdate': 'true' }
+# Limit on inbound rules per Security Group (default: 50)
+RULES_PER_SG = 50
+ 
+def lambda_handler(event, context):
+    # Load the IP addresses from the SOURCE URL
+    addresses = get_ip_addresses(SOURCE)
+    print('Downloaded ' + str(len(addresses)) + ' IP addresses from ' + SOURCE + 'to whitelist')
+    
+    # Find available security groups based on the SECURITY_GROUP_TAGS
+    client = boto3.client('ec2', region_name='eu-west-1')
+    groups = get_security_groups_for_update(client)
+    print ('Found ' + str(len(groups)) + ' security groups to use')
+    
+    # update the security groups
+    result = update_security_groups(client, groups, addresses)
+    
+    return result
+    
+def get_ip_addresses(url):
+    response = urllib2.urlopen(url)
+    ip_list = response.read()
+    
+    return [ip.strip() for ip in ip_list.splitlines()]
+
+def get_security_groups_for_update(client):
+    filters = list();
+    for key, value in SECURITY_GROUP_TAGS.iteritems():
+        filters.extend(
+            [
+                { 'Name': "tag-key", 'Values': [ key ] },
+                { 'Name': "tag-value", 'Values': [ value ] }
+            ]
+        )
+    response = client.describe_security_groups(Filters=filters)
+    
+    return response['SecurityGroups']
+    
+def update_security_groups(client, groups, addresses):
+    result = list()
+    groupsupdated = 0
+    groupindex = 0
+    ipaddresses = 0
+    for group in groups:
+        start = groupindex * RULES_PER_SG / 2
+        end = start + RULES_PER_SG / 2
+        addrsubset = addresses[start:end]
+        print('Adding ' + str(len(addrsubset)) + ' IP addresses to ' + group['GroupId'])
+        clear_security_group(client, group)
+        if update_security_group(client, group, addrsubset):
+            groupindex += 1
+            groupsupdated += 1
+            result.append('Updated ' + group['GroupId'])
+    result.append('Updated ' + str(groupsupdated) + ' of ' + str(len(groups)) + ' security groups')
+    
+    return result
+
+def clear_security_group(client, group):
+    removed = 0
+    if len(group['IpPermissions']) > 0:
+        for permission in group['IpPermissions']:
+            to_revoke = list()
+            for range in permission['IpRanges']:
+                cidr = range['CidrIp']
+                to_revoke.append(range)
+            removed += revoke_permissions(client, group, permission, to_revoke)
+        print (group['GroupId'] + ": Revoked " + str(removed) + ' rules')
+        
+    return(1)
+
+def update_security_group(client, group, addresses):
+    added = 0
+    for port in INGRESS_PORTS:
+        to_add = list()
+        for address in addresses:
+            range = address + "/32"
+            to_add.append({ 'CidrIp': range })
+        permission = { 'ToPort': port, 'FromPort': port, 'IpProtocol': 'tcp'}
+        added += add_permissions(client, group, permission, to_add)
+    print(group['GroupId'] + ": Added " + str(added) + ' rules')
+    
+    return(added > 0)
+ 
+def revoke_permissions(client, group, permission, to_revoke):
+    if len(to_revoke) > 0:
+        revoke_params = {
+            'ToPort': permission['ToPort'],
+            'FromPort': permission['FromPort'],
+            'IpRanges': to_revoke,
+            'IpProtocol': permission['IpProtocol']
+        }
+        client.revoke_security_group_ingress(GroupId=group['GroupId'], IpPermissions=[revoke_params])
+        
+    return len(to_revoke)
+
+def add_permissions(client, group, permission, to_add):
+    if len(to_add) > 0:
+        add_params = {
+            'ToPort': permission['ToPort'],
+            'FromPort': permission['FromPort'],
+            'IpRanges': to_add,
+            'IpProtocol': permission['IpProtocol']
+        }
+        client.authorize_security_group_ingress(GroupId=group['GroupId'], IpPermissions=[add_params])
+        
+    return len(to_add)
